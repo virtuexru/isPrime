f6554b819de0eb66a4ac4929e3eb41320816d3fb
Leon Shalimov <virtuexru@gmail.com>
2014-11-24 10:10:07 -0500

    big bang init

---
+++ isPrime.py
@@ -0,0 +1,33 @@
+import math
+
+def isPrime(num):
+  if num == 2:
+    return True
+  elif num < 2 or not num % 2 or (num != 3 and not num % 3):
+    return False
+
+  for i in range(3, int(num ** .5 + 1), 2):
+    if not num % i:
+      return False
+
+  return True
+
+def genPrimes(n):  
+  primes = [2,]
+  count = 1
+  testNum = 3
+
+  while count < n:
+    if isPrime(testNum):
+      primes.append(testNum)
+      count += 1
+    testNum += 2
+
+  return primes
+
+import time
+start = time.time()
+all_primes = genPrimes(1000)
+print all_primes
+print sum(all_primes)
+print time.time()-start, 'seconds.'
